83df4e941798dd59ab8c54722dc8d72130a3aa20
Soham Sarkar <54991957+soham-sarkar@users.noreply.github.com>
2021-05-10 15:59:19 +0530

    Update form.js

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './form.css';
 
-const backendUrl = "http://localhost:8081/memes";
+const backendUrl = "http://xmeme-soh.herokuapp.com/memes";
 
 class Form extends Component {
     constructor(props) {
